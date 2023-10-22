5ca2fdb372317ba608475e2158a38c8690b6e2bb
deepredk <deepred.kim@gmail.com>
2023-10-22 21:24:14 +0900

    feat: Entity

---
+++ src/main/java/kr/co/fastcampus/travel/entity/Trip.java
@@ -0,0 +1,37 @@
+package kr.co.fastcampus.travel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private boolean isForeign;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "trip")
+    private List<Itinerary> itineraries;
+}
