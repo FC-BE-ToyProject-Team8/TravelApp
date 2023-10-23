41fe0efe8f816bd5c00699e00ad398b300be8959
deepredk <deepred.kim@gmail.com>
2023-10-23 15:43:17 +0900

    fix: ResponseBody 하나로 통일

--- src/main/java/kr/co/fastcampus/travel/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/kr/co/fastcampus/travel/common/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package kr.co.fastcampus.travel.common.exception.handler;
 
 import kr.co.fastcampus.travel.common.exception.BaseException;
-import kr.co.fastcampus.travel.common.response.BaseResponseBody;
+import kr.co.fastcampus.travel.common.response.ResponseBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,29 +12,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BaseException.class)
-    public ResponseEntity<BaseResponseBody> handleBaseException(BaseException e) {
+    public ResponseEntity<ResponseBody<Void>> handleBaseException(BaseException e) {
         return new ResponseEntity<>(
-            BaseResponseBody.fail(e.getMessage()),
+            ResponseBody.fail(e.getMessage()),
             HttpStatus.OK
         );
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity<BaseResponseBody> handleValidException(
+    public ResponseEntity<ResponseBody<Void>> handleValidException(
         MethodArgumentNotValidException e
     ) {
         return new ResponseEntity<>(
-            BaseResponseBody.fail(e.getMessage()),
+            ResponseBody.fail(e.getMessage()),
             HttpStatus.BAD_REQUEST
         );
     }
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<BaseResponseBody> handleUnexpectedException(
+    public ResponseEntity<ResponseBody<Void>> handleUnexpectedException(
         Exception e
     ) {
         return new ResponseEntity<>(
-            BaseResponseBody.error(e.getMessage()),
+            ResponseBody.error(e.getMessage()),
             HttpStatus.INTERNAL_SERVER_ERROR
         );
     }
