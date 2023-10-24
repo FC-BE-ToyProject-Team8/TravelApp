05e23a2b30291c6ff15452617e4ea0d3863b0733
deepredk <deepred.kim@gmail.com>
2023-10-24 15:16:18 +0900

    fix: `@Slf4j` Service와 Controller에 추가
    # Conflicts:
    #	src/main/java/kr/co/fastcampus/travel/controller/TravelController.java

--- src/main/java/kr/co/fastcampus/travel/controller/TravelController.java
+++ src/main/java/kr/co/fastcampus/travel/controller/TravelController.java
@@ -10,11 +10,13 @@ import kr.co.fastcampus.travel.controller.response.TripResponse;
 import kr.co.fastcampus.travel.entity.Trip;
 import kr.co.fastcampus.travel.service.TripService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
--- src/main/java/kr/co/fastcampus/travel/service/TripService.java
+++ src/main/java/kr/co/fastcampus/travel/service/TripService.java
@@ -6,8 +6,10 @@ import kr.co.fastcampus.travel.controller.request.TripRequest;
 import kr.co.fastcampus.travel.entity.Trip;
 import kr.co.fastcampus.travel.repository.TripRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TripService {
