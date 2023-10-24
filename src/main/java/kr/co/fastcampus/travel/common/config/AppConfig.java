da5ec730528c94312e10397298e5a955ec988c24
JuHyeonPark <parkjh080@naver.com>
2023-10-24 18:18:43 +0900

    refactor: 프로젝트 설정 수정
    프로젝트 설정 수정

--- src/main/java/kr/co/fastcampus/travel/common/config/AppConfig.java
+++ src/main/java/kr/co/fastcampus/travel/common/config/AppConfig.java
@@ -2,6 +2,8 @@ package kr.co.fastcampus.travel.common.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,8 @@ public class AppConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
