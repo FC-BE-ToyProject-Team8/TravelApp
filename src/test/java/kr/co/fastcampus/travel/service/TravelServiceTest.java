a45bb2ff747ae56fa5bcba4f520f756cb5ed951e
hyeond0 <a98792530@gmail.com>
2023-10-24 15:59:31 +0900

    feat: 여행 목록 조회 서비스 테스트 구현
    - Mockito를 이용한 Service 테스트 구현

---
+++ src/test/java/kr/co/fastcampus/travel/service/TravelServiceTest.java
@@ -0,0 +1,55 @@
+package kr.co.fastcampus.travel.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import kr.co.fastcampus.travel.entity.Trip;
+import kr.co.fastcampus.travel.repository.TripRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TravelServiceTest {
+
+    @InjectMocks
+    private TravelService travelService;
+
+    @Mock
+    private TripRepository tripRepository;
+
+    @Test
+    @DisplayName("여행 목록 조회")
+    void findAll() {
+        // given
+        Trip trip1 = Trip.builder()
+            .name("name1")
+            .startDate(LocalDate.of(2023, 1, 1))
+            .endDate(LocalDate.of(2023, 1, 7))
+            .isForeign(true)
+            .build();
+        Trip trip2 = Trip.builder()
+            .name("name2")
+            .startDate(LocalDate.of(2023, 1, 1))
+            .endDate(LocalDate.of(2023, 1, 7))
+            .isForeign(true)
+            .build();
+        List<Trip> trips = new ArrayList<>();
+        trips.add(trip1);
+        trips.add(trip2);
+        given(tripRepository.findAll()).willReturn(trips);
+
+        // when
+        List<Trip> findTrips = travelService.findAllTrips();
+
+        // then
+        assertThat(findTrips.size()).isEqualTo(2);
+        assertThat(findTrips.get(0).getName()).isEqualTo("name1");
+    }
+}
