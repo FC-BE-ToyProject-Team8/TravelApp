8bda32b2b7baf04cfbac37de3489b0d5b51224bd
hyeond0 <a98792530@gmail.com>
2023-10-24 16:28:11 +0900

    fix: 여행 목록 조회 레포지토리 테스트 파일명 수정
    - 여행 목록 조회 Repository 테스트 파일명 수정

--- src/test/java/kr/co/fastcampus/travel/repository/TripRepositoryTest.java
+++ src/test/java/kr/co/fastcampus/travel/repository/TripRepositoryTest.java
@@ -3,6 +3,7 @@ package kr.co.fastcampus.travel.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.stream.IntStream;
 import kr.co.fastcampus.travel.entity.Itinerary;
 import kr.co.fastcampus.travel.entity.Trip;
@@ -65,5 +66,25 @@ class TripRepositoryTest {
         assertThat(result).isNotNull();
         assertThat(result.getId()).isEqualTo(trip.getId());
         assertThat(result.getItineraries().size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("여행 목록 조회")
+    void findAll() {
+        // given
+        Trip trip = Trip.builder()
+            .name("name1")
+            .startDate(LocalDate.of(2023, 1, 1))
+            .endDate(LocalDate.of(2023, 1, 7))
+            .isForeign(true)
+            .build();
+        Trip saveTrip = tripRepository.save(trip);
+
+        // when
+        List<Trip> trips = tripRepository.findAll();
+
+        // then
+        assertThat(trips.size()).isEqualTo(1);
+        assertThat(trips.get(0)).isEqualTo(saveTrip);
     }
 }
