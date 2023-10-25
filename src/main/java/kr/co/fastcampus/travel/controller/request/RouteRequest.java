005ce2cc115fab418d6d6933c8549318c5bec3e3
JuHyeonPark <parkjh080@naver.com>
2023-10-25 20:34:56 +0900

    feat: 이동요청DTO 작성
    이동요청DTO 작성

---
+++ src/main/java/kr/co/fastcampus/travel/controller/request/RouteRequest.java
@@ -0,0 +1,21 @@
+package kr.co.fastcampus.travel.controller.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record RouteRequest (
+        String transportation,
+        String departurePlaceName,
+        String departureAddress,
+        String destinationPlaceName,
+        String destinationAddress,
+        @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+        LocalDateTime departureAt,
+        @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+        LocalDateTime arriveAt
+){
+
+}
