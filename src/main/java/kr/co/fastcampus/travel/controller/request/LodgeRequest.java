4841ca38636e65fbdaee37f723bd4b41d8a68572
JuHyeonPark <parkjh080@naver.com>
2023-10-25 20:34:45 +0900

    feat: 숙박요청DTO 작성
    숙박요청DTO 작성

---
+++ src/main/java/kr/co/fastcampus/travel/controller/request/LodgeRequest.java
@@ -0,0 +1,18 @@
+package kr.co.fastcampus.travel.controller.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record LodgeRequest (
+        String placeName,
+        String address,
+        @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+        LocalDateTime checkInAt,
+        @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+        LocalDateTime checkOutAt
+){
+
+}
