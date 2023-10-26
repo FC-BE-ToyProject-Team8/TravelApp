c461194c19e31b2d11147e351e33d69c1334b8b2
JuHyeonPark <parkjh080@naver.com>
2023-10-26 11:53:53 +0900

    test: 여정 복수 등록 컨트롤러 테스트 코드 작성
    여정 복수 등록 컨트롤러 테스트 코드 작성

---
+++ src/test/java/kr/co/fastcampus/travel/controller/TravelControllerTest.java
@@ -0,0 +1,84 @@
+package kr.co.fastcampus.travel.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kr.co.fastcampus.travel.controller.request.ItineraryRequest;
+import kr.co.fastcampus.travel.controller.response.TripResponse;
+import kr.co.fastcampus.travel.entity.Trip;
+import kr.co.fastcampus.travel.repository.ItineraryRepository;
+import kr.co.fastcampus.travel.repository.TripRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static kr.co.fastcampus.travel.testUtil.createMockItineraryRequest;
+import static kr.co.fastcampus.travel.testUtil.createMockTrip;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TravelControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @Autowired
+    private ItineraryRepository itineraryRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+
+    @Test
+    @DisplayName("여정 복수 등록")
+    void addItineraries() {
+        // given
+        Trip trip = createMockTrip();
+        tripRepository.save(trip);
+        String url = "/api/trips/1/itineraries";
+        List<ItineraryRequest> request = IntStream.range(0, 3)
+                .mapToObj(i -> createMockItineraryRequest())
+                .toList();
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post(url)
+                .then().log().all()
+                .extract();
+
+        // then
+        JsonPath jsonPath = response.jsonPath();
+        String status = jsonPath.getString("status");
+        TripResponse data = jsonPath.getObject("data", TripResponse.class);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        assertSoftly((softly) -> {
+            softly.assertThat(status).isEqualTo("SUCCESS");
+            softly.assertThat(data.id()).isEqualTo(1);
+            softly.assertThat(data.name()).isEqualTo("tripName");
+            softly.assertThat(data.itineraries().size()).isEqualTo(3);
+        });
+    }
+}
