f2170b25a2c379398d0688748b1bf7cbac6aeb38
ChangHo <tony9911@naver.com>
2023-10-26 19:50:49 +0900

    refactor: 중복 사용 코드 메소드로 추출

--- src/main/java/kr/co/fastcampus/travel/service/ItineraryService.java
+++ src/main/java/kr/co/fastcampus/travel/service/ItineraryService.java
@@ -2,25 +2,32 @@ package kr.co.fastcampus.travel.service;
 
 import static kr.co.fastcampus.travel.controller.util.TravelDtoConverter.toItinerary;
 
+import jakarta.transaction.Transactional;
 import kr.co.fastcampus.travel.common.exception.EntityNotFoundException;
 import kr.co.fastcampus.travel.controller.request.ItineraryRequest;
 import kr.co.fastcampus.travel.entity.Itinerary;
 import kr.co.fastcampus.travel.repository.ItineraryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ItineraryService {
 
     private final ItineraryRepository itineraryRepository;
 
+    public Itinerary findByItineraryId(Long itineraryId) {
+        return itineraryRepository.findById(itineraryId)
+            .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Transactional
     public Itinerary editItinerary(Long id, ItineraryRequest request) {
-        Itinerary itinerary =
-            itineraryRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        Itinerary itinerary = findByItineraryId(id);
         Itinerary itineraryToBeUpdated = toItinerary(request);
         itinerary.update(itineraryToBeUpdated);
         return itineraryRepository.save(itinerary);
     }
-
 }
