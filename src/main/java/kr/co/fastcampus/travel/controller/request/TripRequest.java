6c2503c30d1f99f8d54f53a52c6ce0ebae262942
Jinhong Kim <deepred.kim@gmail.com>
2023-10-26 15:27:41 +0900

    fix: TripRequest 에러 메시지 잘 나오게끔 수정

--- src/main/java/kr/co/fastcampus/travel/controller/request/TripRequest.java
+++ src/main/java/kr/co/fastcampus/travel/controller/request/TripRequest.java
@@ -3,21 +3,20 @@ package kr.co.fastcampus.travel.controller.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import java.time.LocalDate;
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Builder
 public record TripRequest(
     @NotBlank
     String name,
     @NotNull
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
-    String startDate,
+    LocalDate startDate,
     @NotNull
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
-    String endDate,
+    LocalDate endDate,
     @NotNull
-    @Pattern(regexp = "true|false", message = "값은 true 또는 false 여야 합니다.")
-    boolean isForeign
+    Boolean isForeign
 ) {
 
 }
