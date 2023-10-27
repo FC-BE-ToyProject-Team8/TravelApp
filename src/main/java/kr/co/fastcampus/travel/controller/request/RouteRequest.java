3076c2e406d09aa99331f532fbc7dbd292c5bb7b
JuHyeonPark <parkjh080@naver.com>
2023-10-27 11:06:18 +0900

    refactor: 리뷰 내용 반영
    이동, 숙박, 체류 요청 DTO에 날짜 패턴 지움

--- src/main/java/kr/co/fastcampus/travel/controller/request/RouteRequest.java
+++ src/main/java/kr/co/fastcampus/travel/controller/request/RouteRequest.java
@@ -1,6 +1,6 @@
 package kr.co.fastcampus.travel.controller.request;
 
-import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDateTime;
 import lombok.Builder;
 
@@ -11,9 +11,9 @@ public record RouteRequest(
     String departureAddress,
     String destinationPlaceName,
     String destinationAddress,
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+
     LocalDateTime departureAt,
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+
     LocalDateTime arriveAt
 ) {
 
--- src/main/java/kr/co/fastcampus/travel/controller/request/StayRequest.java
+++ src/main/java/kr/co/fastcampus/travel/controller/request/StayRequest.java
@@ -1,6 +1,5 @@
 package kr.co.fastcampus.travel.controller.request;
 
-import jakarta.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
@@ -8,9 +7,9 @@ import lombok.Builder;
 public record StayRequest(
     String placeName,
     String address,
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+
     LocalDateTime startAt,
-    @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2})$", message = "날짜 형식은 yyyy-MM-dd 여야 합니다.")
+
     LocalDateTime endAt
 ) {
 
