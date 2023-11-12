9418d4f8820c22ff042628c4963b196e7191a422
cyPark95 <parkcy1007@gmail.com>
2023-11-12 17:39:45 +0900

    feat: Spring Security 필터 등록

---
+++ src/main/java/kr/co/fastcampus/travel/common/secure/config/SecurityConfigAdapter.java
@@ -0,0 +1,24 @@
+package kr.co.fastcampus.travel.common.secure.config;
+
+import kr.co.fastcampus.travel.common.secure.domain.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class SecurityConfigAdapter
+        extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>
+{
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        builder.addFilterBefore(
+                new SecurityFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
