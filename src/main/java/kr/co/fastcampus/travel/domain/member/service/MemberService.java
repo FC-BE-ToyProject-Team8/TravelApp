7538def4c6ea36ea096055d5d3c7580e42544565
cyPark95 <parkcy1007@gmail.com>
2023-11-12 13:07:55 +0900

    feat: 사용자 저장 로직 구현 & 테스트
    - 이메일 중복 검증
    - 이미 존재하는 이메일의 경우 `InvalidArgumentException` 발생한다.
    - 닉네임 중복 검증
    - 이미 존재하는 닉네임의 경우 `InvalidArgumentException` 발생한다.
    
    [테스트 케이스]
    - 이메일 중복에 대해 `InvalidArgumentException` 발생 검증
    - 닉네임 중복에 대해 `InvalidArgumentException` 발생 검증
    - 사용자 저장 로직 실행 검증

---
+++ src/main/java/kr/co/fastcampus/travel/domain/member/service/MemberService.java
@@ -0,0 +1,38 @@
+package kr.co.fastcampus.travel.domain.member.service;
+
+import kr.co.fastcampus.travel.common.exception.InvalidArgumentException;
+import kr.co.fastcampus.travel.domain.member.entity.Member;
+import kr.co.fastcampus.travel.domain.member.repository.MemberRepository;
+import kr.co.fastcampus.travel.domain.member.service.dto.request.MemberSaveDto;
+import kr.co.fastcampus.travel.domain.member.service.dto.response.MemberDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberDto save(MemberSaveDto dto) {
+        validateSaveMember(dto);
+        var encodePassword = passwordEncoder.encode(dto.password());
+        Member member = dto.toEntity(encodePassword);
+        memberRepository.save(member);
+        return MemberDto.from(member);
+    }
+
+    private void validateSaveMember(MemberSaveDto dto) {
+        if (memberRepository.findByEmail(dto.email()).isPresent()) {
+            throw new InvalidArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        if (memberRepository.findByNickname(dto.nickname()).isPresent()) {
+            throw new InvalidArgumentException("이미 존재하는 닉네임입니다.");
+        }
+    }
+}
