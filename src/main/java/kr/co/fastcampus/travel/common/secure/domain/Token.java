c757ea81374524afbaa5489c9dac079a2ba11df1
cyPark95 <parkcy1007@gmail.com>
2023-11-13 17:16:03 +0900

    feat: 로그인 Controller 기능 구현 & 통합 테스트
    - 로그인 시, 이메일과 패스워드는 필수이다.
    
    [테스트 케이스]
    - 존재하지 않는 사용자 로그인에 대해 HttpStatus `400` 반환 검증
    - 잘못된 비밀번호 로그인에 대해 HttpStatus `400` 반환 검증
    - 정상 요청에 대해 토큰 반환 검증

--- src/main/java/kr/co/fastcampus/travel/common/secure/domain/Token.java
+++ src/main/java/kr/co/fastcampus/travel/common/secure/domain/Token.java
@@ -4,10 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class Token {
 
   private String grantType;
   private String accessToken;
   private String refreshToken;
+
+  @Builder
+  private Token(String grantType, String accessToken, String refreshToken) {
+    this.grantType = grantType;
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
 }
