dd64d14e7287358ddd07bc61d1c4cb1e86518ba5
cyPark95 <parkcy1007@gmail.com>
2023-11-13 17:16:24 +0900

    feat: 토큰을 필요로 하는 검증에 대한 Utils 작성

---
+++ src/test/java/kr/co/fastcampus/travel/common/RestAssuredUtils.java
@@ -0,0 +1,55 @@
+package kr.co.fastcampus.travel.common;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.http.MediaType;
+
+public final class RestAssuredUtils {
+
+    private RestAssuredUtils() {
+    }
+
+    public static ExtractableResponse<Response> restAssuredPostBody(String url, Object request) {
+        return restAssured()
+                .body(request)
+                .when()
+                .post(url)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> restAssuredGetWithToken(String url) {
+        String accessToken = TokenUtils.getAccessToken();
+
+        return restAssuredWithToken(accessToken)
+                .when()
+                .get(url)
+                .then().log().all()
+                .extract();
+    }
+
+
+    public static ExtractableResponse<Response> restAssuredPostWithToken(String url, Object request) {
+        String accessToken = TokenUtils.getAccessToken();
+
+        return restAssuredWithToken(accessToken)
+                .body(request)
+                .when()
+                .post(url)
+                .then().log().all()
+                .extract();
+    }
+
+    private static RequestSpecification restAssuredWithToken(String accessToken) {
+        return restAssured()
+                .header("Authorization", accessToken);
+    }
+
+    private static RequestSpecification restAssured() {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+    }
+}
---
+++ src/test/java/kr/co/fastcampus/travel/common/TokenUtils.java
@@ -0,0 +1,30 @@
+package kr.co.fastcampus.travel.common;
+
+import static kr.co.fastcampus.travel.common.MemberTestUtils.EMAIL;
+import static kr.co.fastcampus.travel.common.MemberTestUtils.PASSWORD;
+import static kr.co.fastcampus.travel.common.MemberTestUtils.createMemberSaveReqeust;
+import static kr.co.fastcampus.travel.common.RestAssuredUtils.restAssuredPostBody;
+
+import kr.co.fastcampus.travel.domain.member.controller.dto.request.MemberSaveRequest;
+import kr.co.fastcampus.travel.domain.secure.controller.dto.reqeust.LoginReqeust;
+
+public final class TokenUtils {
+
+    public static final String GRANT_TYPE = "Bearer ";
+
+    private TokenUtils() {
+    }
+
+    public static String getAccessToken() {
+        createMember();
+        String url = "/login";
+        LoginReqeust request = new LoginReqeust(EMAIL, PASSWORD);
+        return GRANT_TYPE + restAssuredPostBody(url, request).jsonPath().getString("data.accessToken");
+    }
+
+    private static void createMember() {
+        String url = "/signup";
+        MemberSaveRequest request = createMemberSaveReqeust();
+        restAssuredPostBody(url, request);
+    }
+}
