00127def08b53c1f94463e901e65a5f3a6f75fff
JuHyeonPark <parkjh080@naver.com>
2023-11-14 01:15:17 +0900

    refactor: 닉네임으로 Member 찾는 메소드 구현
    닉네임으로 Member 찾는 메소드 구현

--- src/main/java/kr/co/fastcampus/travel/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/co/fastcampus/travel/domain/member/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package kr.co.fastcampus.travel.domain.member.repository;
 import kr.co.fastcampus.travel.domain.member.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    Optional<Member> findByNickname(String nickname);
 }
