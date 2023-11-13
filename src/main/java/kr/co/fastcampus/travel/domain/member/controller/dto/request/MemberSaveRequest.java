a2d7bf6afa0f82c5ac1e17caf64851e9105a5a92
cyPark95 <parkcy1007@gmail.com>
2023-11-13 15:00:52 +0900

    feat: 회원가입 파라미터 유효성 에러 메시지 지정

--- src/main/java/kr/co/fastcampus/travel/domain/member/controller/dto/request/MemberSaveRequest.java
+++ src/main/java/kr/co/fastcampus/travel/domain/member/controller/dto/request/MemberSaveRequest.java
@@ -3,13 +3,13 @@ package kr.co.fastcampus.travel.domain.member.controller.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record MemberSaveRequest(
-        @NotBlank
+        @NotBlank(message = "이메일은 필수 입력 사항입니다.")
         String email,
-        @NotBlank
+        @NotBlank(message = "이름은 필수 입력 사항입니다.")
         String name,
-        @NotBlank
+        @NotBlank(message = "닉네임은 필수 입력 사항입니다.")
         String nickname,
-        @NotBlank
+        @NotBlank(message = "패스워드는 필수 입력 사항입니다.")
         String password
 ) {
 
