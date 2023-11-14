32677b2f126f9215e2a5d77d11027930fa3e066b
JuHyeonPark <parkjh080@naver.com>
2023-11-14 17:37:53 +0900

    refactor: 엔티티 dto 빌더 수정
    엔티티 dto 빌더 수정

--- src/main/java/kr/co/fastcampus/travel/domain/like/service/dto/LikeSaveDto.java
+++ src/main/java/kr/co/fastcampus/travel/domain/like/service/dto/LikeSaveDto.java
@@ -13,7 +13,6 @@ public record LikeSaveDto(
 ) {
     public Like toEntity(){
         return Like.builder()
-            .id(id)
             .member(member)
             .trip(trip)
             .build();
