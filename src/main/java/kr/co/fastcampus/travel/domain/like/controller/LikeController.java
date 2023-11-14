06a388002a6b9204d00eea53cb75a7408611240a
JuHyeonPark <parkjh080@naver.com>
2023-11-14 17:49:28 +0900

    feat: Like mvc 계층 구현
    Like mvc 계층 구현

--- src/main/java/kr/co/fastcampus/travel/domain/like/controller/LikeController.java
+++ src/main/java/kr/co/fastcampus/travel/domain/like/controller/LikeController.java
@@ -1,5 +1,6 @@
 package kr.co.fastcampus.travel.domain.like.controller;
 
+import java.security.Principal;
 import kr.co.fastcampus.travel.common.response.ResponseBody;
 import kr.co.fastcampus.travel.domain.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @Slf4j
 @RestController
@@ -21,8 +20,8 @@ public class LikeController {
 
     @PostMapping
     public ResponseBody<Void> likeTrip(@RequestParam("tripId") Long tripId, Principal principal) {
-        Long memberId = Long.parseLong(principal.getName());
-        likeService.saveLike(tripId, memberId);
+        String memberEmail = principal.getName();
+        likeService.saveLike(tripId, memberEmail);
         return ResponseBody.ok();
     }
 }
