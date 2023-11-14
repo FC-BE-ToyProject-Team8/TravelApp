75d25996a99f1446df96ec5d1e3751659ac73d96
JuHyeonPark <parkjh080@naver.com>
2023-11-14 17:40:06 +0900

    feat: 예외처리 추가
    예외처리 추가

--- src/main/java/kr/co/fastcampus/travel/common/response/ErrorCode.java
+++ src/main/java/kr/co/fastcampus/travel/common/response/ErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode {
     COMMON_ENTITY_NOT_FOUND("존재하지 않는 엔티티입니다."),
-    DUPLICATED_LIKE("중복된 좋아요입니다.");
+    DUPLICATED_LIKE("중복된 좋아요입니다."),
+    MEMBER_NOT_FOUND("존재하지 않는 사용자입니다.");
 
 
     private final String errorMsg;
