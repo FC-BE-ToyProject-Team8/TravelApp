5fc79ac00ada8d3e7244170368ffcdc36fe856d1
Chang Ho Moon <87019291+windowM@users.noreply.github.com>
2023-11-15 17:13:09 +0900

    chore : 머지 충돌 해결 과정 추가된 라이브러리 제거

--- src/main/java/kr/co/fastcampus/travel/domain/trip/entity/Trip.java
+++ src/main/java/kr/co/fastcampus/travel/domain/trip/entity/Trip.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import kr.co.fastcampus.travel.common.baseentity.BaseEntity;
 import kr.co.fastcampus.travel.domain.comment.entity.Comment;
-import kr.co.fastcampus.travel.common.exception.InvalidDateSequenceException;
 
 import kr.co.fastcampus.travel.domain.itinerary.entity.Itinerary;
 import kr.co.fastcampus.travel.domain.member.entity.Member;
