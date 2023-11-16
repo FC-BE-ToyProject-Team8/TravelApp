aba83cd9b0f759355b26bfd66042d928e0adbf9e
deepredk <deepred.kim@gmail.com>
2023-11-16 10:42:17 +0900

    fix: 컴파일 오류 수정

--- src/main/java/kr/co/fastcampus/travel/common/response/ErrorCode.java
+++ src/main/java/kr/co/fastcampus/travel/common/response/ErrorCode.java
@@ -15,8 +15,8 @@ public enum ErrorCode {
     MEMBER_MISMATCH("작성자가 아니므로 해당 권한이 없습니다."),
     INVALID_LIKE_CANCEL("잘못된 좋아요 취소입니다."),
     API_REQUEST_FAILED("API 요청에 실패하였습니다."),
-    API_UNAUTHORIZED("API 인증에 실패하였습니다. API 키를 다시 확인해주세요."),
-    MEMBER_MISMATCH("작성자가 아니므로 해당 권한이 없습니다.");
+    API_UNAUTHORIZED("API 인증에 실패하였습니다. API 키를 다시 확인해주세요.")
+    ;
 
     private final String errorMsg;
 
