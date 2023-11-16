0888007ee754f2eff9a4e2d5e0fd125d74197ee5
JuHyeonPark <parkjh080@naver.com>
2023-11-16 15:47:25 +0900

    refactor: 이메일로 사용자 찾기 통일화 및 사용자 없음 에러코드 통일
    이메일로 사용자 찾기 통일화 및 사용자 없음 에러코드 통일

--- src/main/java/kr/co/fastcampus/travel/domain/trip/service/TripService.java
+++ src/main/java/kr/co/fastcampus/travel/domain/trip/service/TripService.java
@@ -43,7 +43,7 @@ public class TripService {
     }
 
     private Member findMember(String memberEmail) {
-        return memberService.findByEmail(memberEmail);
+        return memberService.findMemberByEmail(memberEmail);
     }
 
     public TripItineraryInfoDto findTripItineraryById(Long id) {
